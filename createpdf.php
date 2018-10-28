52c7373fad7aeb6ad89761fc8247c7ad4861f840
vikasmahato <vikasmahato0@gmail.com>
2018-10-28 10:33:35 +0530

    Added skeleton for creating pdf

--- createpdf.php
+++ createpdf.php
@@ -16,7 +16,7 @@ require_once 'dompdf/autoload.inc.php';
 use Dompdf\Dompdf;
 
  $id = $_POST['id'];
- $sql = "SELECT * FROM form WHERE app_id = $id";
+ $sql = "SELECT * FROM order WHERE id = $id";
 // instantiate and use the dompdf class
 $result=mysqli_query($con, $sql);
 $name ="";
@@ -28,55 +28,22 @@ if ($result = mysqli_query($con, $sql)) {
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
         $value = $row;
-       //echo $value["app_id"]." ".$value['applicant_name']." ".$value['a_cghs_no'];
-     //   generate( $value, '2');
-        
-        if (isset($_POST['form1_btn'])){generate($value, '1');}
-         //else if (isset($_POST['form10_btn'])) {echo "form 10";}
-       else if (isset($_POST['form2_btn'])) {generate($value, '2');}
-          else if (isset($_POST['form3_btn'])) {generate($value, '3');}
-           else if (isset($_POST['form4_btn'])) {generate($value, '4');}
-           else if (isset($_POST['form5_btn'])) {echo "form 5";}
-           else if (isset($_POST['form6_btn'])) {echo "form 6";}
-           else if (isset($_POST['form7_btn'])) {generate($value, '7');}
-           else if (isset($_POST['form8_btn'])) {echo "form 8";}
-           else if (isset($_POST['form9_btn'])) {generate($value, '9');}
-          else if (isset($_POST['form10_btn'])) {generate($value, '10');}
-           else if (isset($_POST['form11_btn'])) {echo "form 11";}
-           else if (isset($_POST['form12_btn'])) {generate($value, '12');}
-           else if (isset($_POST['form13_btn'])) {generate($value, '13');}
-           else if (isset($_POST['form14_btn'])) {echo "form 14";}
-           else if (isset($_POST['form15_btn'])) {generate($value, '15');}
-           else if (isset($_POST['form16_btn'])) {echo "form 16";}
-        else if (isset($_POST['form22_btn'])) {generate($value, '22');}
+        generate($value);
     }
 
     /* free result set */
     $result->free();
 }
 
-function generate( $value, $s){
+function generate($value){
     $rupee_word = rupee_to_word($value['amt_granted']);
-    $data = get_data_for_calcSheet($value['app_id']);
-    require 'docs/form1.php';
-    require 'docs/form2.php';
-      require 'docs/form3.php';
-      require 'docs/form4.php';
-    require 'docs/form10.php';
-    require 'docs/form12.php';
-    require 'docs/form13.php';
-    require 'docs/form15.php';
-     require 'docs/form7.php';
-    require 'docs/form22.php';
-//require 'docs/form4.php';
+    require 'docs/orderPDF.php';
 
-  //  $html = 'vikas says hello world dsad'.$value['applicant_name'];
-    
     $dompdf = new Dompdf();
-$dompdf->loadHtml(${'form'.$s});
-$dompdf->setPaper('A4', 'portrait');
-$dompdf->render();
-$dompdf->stream("file".$s.".pdf");
+    $dompdf->loadHtml($orderPDF);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
+    $dompdf->stream("file.pdf");
 
 }
 
